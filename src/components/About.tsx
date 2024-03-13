3243772358f0f5c35913e00b4b7471bc90210186
Senath Sethmika <94299314+senath112@users.noreply.github.com>
2024-03-13 11:32:12 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion as m } from 'framer-motion';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Marquee from "react-fast-marquee";
-import { Link } from 'react-router-dom';
-import { motion as m } from 'framer-motion'
-type Props = {}
-function About({ }: Props) {
+
+function About() {
   return (
     <section>
       <Marquee
@@ -30,23 +30,18 @@ function About({ }: Props) {
         <div className='flex flex-col gap-7 md:flex-col lg:flex-row'>
           <div className='md:flex md:justify-center'>
             {/* card 1 */}
-            <m.div
-              className="flex">
-              <div
-                className="flex flex-col gap-5 justify-center lg:w-[45rem] md:w-[31.6rem] rounded-lg bg-30 px-10 pt-5 pb-9">
-                <h5
-                  className="mb-2 text-xl font-medium text-white">
+            <m.div className="flex">
+              <div className="flex flex-col gap-5 justify-center lg:w-[45rem] md:w-[31.6rem] rounded-lg bg-30 px-10 pt-5 pb-9">
+                <h5 className="mb-2 text-xl font-medium text-white">
                   Are you ready to unlock your creativity? <br /> 
                 </h5>
                 <p className="mb-0 text-white font-light">
-                Arduino Day 2023 is just around the corner and we at arduino community srilanka are excited to bring you some 
-                creative content to celebrate!  Register for hackathons, educational seminars and inspiration to help you explore 
-                the endless possibilities of this amazing platform.
+                  Arduino Day 2023 is just around the corner and we at arduino community srilanka are excited to bring you some 
+                  creative content to celebrate!  Register for hackathons, educational seminars and inspiration to help you explore 
+                  the endless possibilities of this amazing platform.
                 </p>
                 <Link to={'/register'}>
-                  <button
-                    type="button"
-                    className="btn-2 mt-5">
+                  <button type="button" className="btn-2 mt-5">
                     Register Now
                   </button>
                 </Link>
@@ -54,28 +49,26 @@ function About({ }: Props) {
             </m.div>
              {/* card 1 */}
            </div>
-
-           <m.div
-
-             className='flex flex-col md:flex-row gap-7'>
-             
-             <m.div
-               className="flex">
-               <div
-                 className="w-[100%] h-[25rem] md:w-[15rem] md:max-h-[20rem] rounded-lg border-30 border-2 px-8 pt-5 pb-9 flex justify-between card-two">
-                 
-              
+           <m.div className='flex flex-col md:flex-row gap-7'>
+             <m.div className="flex">
+               <div className="w-[100%] h-[25rem] md:w-[15rem] md:max-h-[20rem] rounded-lg border-30 border-2 px-8 pt-5 pb-9 flex justify-between card-two">
+                 {/* Card content */}
                </div>
              </m.div>
-            
-
-
              {/* card-3 */}
-             <div className="flex">
-                   className="mb-2 text-xl font-medium text-30">
-                   Speaker <br />Session
-                 </h5>
-                 <Link to={'/daytwo-regulations'}>
-                   <ArrowForwardIcon className='text-30 mt-2' />
-                 </Link>
+             <m.div className="flex">
+               <div className="mb-2 text-xl font-medium text-30">
+                 Speaker <br />Session
                </div>
+               <Link to={'/daytwo-regulations'}>
+                 <ArrowForwardIcon className='text-30 mt-2' />
+               </Link>
+             </m.div>
+          </m.div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
